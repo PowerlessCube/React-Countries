57d53faa55c1cf5a4a109e702a36e7037392d76f
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-06-28 15:16:31 +0100

    Display border codes for selected countries

--- client/src/components/CountriesDisplay.jsx
+++ client/src/components/CountriesDisplay.jsx
@@ -2,10 +2,36 @@ var React = require('react');
 
 var CountryDisplay = React.createClass({
 	render: function() {
+		// var displayBorderElement = <h3> Bordering Countries </h3>
+		// if (this.state.displayCountry.borders.length != 0) {
+		// 	displayBorderElement
+		// }
+
+		var listItems = this.props.country.borders.map( function(country, index) {
+			return <li key={index}> {country} </li>
+		});
+
 		return(
-			//Receives the property from Countries Box 
-			<h4>{this.props.country.name}</h4>
+			//Receives the property from Countries Box
+			<div className="display-country-info">
+				<div>
+					<h3>{this.props.country.name}</h3>
+					<ul>
+						<li className="country-info">Native Name: {this.props.country.nativeName}</li>
+						<li className="country-info">Capital City: {this.props.country.capital}</li>
+						<li className="country-info">Population: {this.props.country.population}</li>
+					</ul>
+				</div>
+
+				<div className="border-country">
+					<h3>Bordering Countries</h3>
+					<ul>
+						{listItems}
+					</ul>
+				</div>
+			</div>
 		)
+		// TODO: Create a new sub component of CountryDisplay, displaying the codes of the bordering countries.
 	}
 })
 
